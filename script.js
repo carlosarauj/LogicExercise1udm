8798933be56cf8c5e9d65fa1b5a84b857026a3f8
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-09-15 21:18:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function maior(x, y){
+    if (x > y){
+        console.log(`${x} é maior do que ${y}`)
+    } else {
+        console.log(`${y} é maior do que ${x}`)
+    }
+
+
+}
+
+maior(3, 6)
+
+//outra maneira:
+/*
+function maior(x, y){
+    return x > y ? x : y
+}
+console.log(maior(10,20))
+
+*/
+
+//arrow function
+/*
+
+
+*/
